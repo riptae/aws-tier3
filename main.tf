91651433aac7bf8aec8ded3dbf0275bb3ab1c508
yun <riptaggart@naver.com>
2026-02-04 09:10:21 +0900

    [1] providers

---
+++ main.tf
@@ -0,0 +1,20 @@
+# [1] providers
+terraform {
+  required_version = ">= 1.5.6"
+
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 5.0"
+    }
+
+    random = {
+        source = "hashicorp/random"
+        version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+    region = "ap-northeast-2"
+}
